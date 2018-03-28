a408bbff65c155dec12e262546ca0282afbbdcfd
navaponpea <37840231+navaponpea@users.noreply.github.com>
2018-03-28 20:26:33 +0700

    commit

--- assets/js/test.js
+++ assets/js/test.js
@@ -2,6 +2,7 @@
 function update_data(id, number) {
     $("#"+String(id)).text(String(number));
 }
+
 function writeUserData(userId, name, email, imageUrl) {
   firebase.database().ref('users/' + userId).set({
     username: name,
@@ -9,22 +10,22 @@ function writeUserData(userId, name, email, imageUrl) {
     profile_picture : imageUrl
   });
 }
+
 $( document ).ready(function() {
     console.log("starting document!!!!");
 
     // Initialize Firebase
     console.log("Initialize Firebase");
     // Initialize Firebase
-
     var config = {
-     apiKey: "AIzaSyB5yHv9dw0E9fw5IoB3-W8B2BEs2g5y4cE",
-     authDomain: "python-training-81cd1.firebaseapp.com",
-     databaseURL: "https://python-training-81cd1.firebaseio.com",
-     projectId: "python-training-81cd1",
-     storageBucket: "python-training-81cd1.appspot.com",
-     messagingSenderId: "976999995725"
-   };
+      apiKey: "AIzaSyB5yHv9dw0E9fw5IoB3-W8B2BEs2g5y4cE",
+      authDomain: "python-training-81cd1.firebaseapp.com",
+      databaseURL: "https://python-training-81cd1.firebaseio.com",
+      projectId: "python-training-81cd1",
+      storageBucket: "python-training-81cd1.appspot.com",
+      messagingSenderId: "976999995725"
 
+    };
     firebase.initializeApp(config);
 
     var ref = firebase.database().ref();
@@ -39,7 +40,8 @@ $( document ).ready(function() {
         } else {
            update_data(data.key, parseInt(data.val()))
 
-             writeUserData('45','navapon','navaoi@gmail.com','https://pbs.twimg.com/profile_images/966896631147765760/AJ836huS_400x400.jpg')
+           writeUserData('navapon', 'navapon nopsuwan',
+           'nnnnn@vt.edu', 'https://pbs.twimg.com/profile_images/966896631147765760/AJ836huS_400x400.jpg')
         }
     });
 
